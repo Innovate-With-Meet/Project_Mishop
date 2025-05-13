e4dc05c486dbc5ba070614445bb8ed042dbd6c2a
Rana Meet S <102943643+Innovate-With-Meet@users.noreply.github.com>
2025-05-13 23:54:33 +0530

    Update App.jsx

--- Mishop_Frontend/src/App.jsx
+++ Mishop_Frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<LandingPage />}> </Route>
         <Route path="/Landing" element={<LandingPage />}> </Route>
       </Routes>
-      {/* <h1></h1> */}
+      {/*  */}
     </>
   )
 }
